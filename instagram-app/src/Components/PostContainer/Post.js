b63767a3e081ac2d9a95c6966661356003159e14
Wes Jonke <wesjonke@gmail.com>
2019-06-04 12:35:47 -0400

    final touches on CSS

--- instagram-app/src/Components/PostContainer/Post.js
+++ instagram-app/src/Components/PostContainer/Post.js
@@ -6,18 +6,38 @@ import './Posts.css';
 
 const Post = props => {
   return (
+
     <div className="post-border">
       <PostHeader
         username={props.post.username}
         thumbnailUrl={props.post.thumbnailUrl}
       />
+
       <div className="post-image-wrapper">
         <img
           alt="post thumbnail"
           className="post-image"
           src={props.post.imageUrl}
         />
+
+      
+      <div className="imgFooter">
+        <div className="icon">
+          <i className="fa fa-heart" />
+        </div>
+        <div className="icon">   
+          <i className="fa fa-comment" />
+        </div>
       </div>
+
+
+      
+
+      <div> 
+        <span className="likes">{props.post.likes} Likes</span>
+      </div>
+      
+    </div>
       <CommentSection comments={props.post.comments} />
     </div>
   );
